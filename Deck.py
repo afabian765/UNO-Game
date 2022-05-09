f47e7a6ab35b22a39dd7f163adc5f305dea839ff
afabian765 <104509938+afabian765@users.noreply.github.com>
2022-05-09 08:48:05 -0500

    Add files via upload

---
+++ Deck.py
@@ -0,0 +1,18 @@
+
+def UNOdeck():
+    deck = []
+    colors = ["Red", "Blue", "Yellow", "Green"]
+    values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Draw Two", "Reverse", "Skip", "Draw Four"]
+    for color in colors:
+        for value in values:
+            cardValue = "{}{}".format(color, value)
+            deck.append(cardValue)
+    print(deck)
+    
+print(UNOdeck())
+
+def UNOcard():   
+    if value == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+        
+    
+
